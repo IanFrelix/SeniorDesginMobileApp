23e245efbb3b0d75421c89d301c6a4f5f7edce74
Ian Frelix <ianfrelix@gmail.com>
2023-02-13 17:08:21 -0500

    Initialcommit3

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>You're now accessing the mobile app for the F.A.R.M. (Stay tuned for updates!)</Text>
       <StatusBar style="auto" />
     </View>
   );
